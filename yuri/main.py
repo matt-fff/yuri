cbbba5df6f85fff7e9e5b362e3817df315cf123e
Matt White <code@typenil.com>
2021-02-15 22:06:58 -0700

    TTS working

--- yuri/main.py
+++ yuri/main.py
@@ -1,8 +1,24 @@
+import os
+from io import BytesIO
+
 import typer
+from loguru import logger
+from gtts import gTTS
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+def say(message: str):
+    typer.echo(message)
+    mp3_fp = BytesIO()
+    tts = gTTS(message, lang="en")
+    tts.write_to_fp(mp3_fp)
+    mp3_fp.seek(0)
+    play(AudioSegment.from_mp3(mp3_fp))
 
 
 def main(name: str):
-    typer.echo(f"Hello {name}")
+    say(f"Hello {name}")
 
 
 if __name__ == "__main__":
